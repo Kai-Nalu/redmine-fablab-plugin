28f6e9877b9c51ac60dc36ae19f8bc724260e0f1
Kai-Nalu <kainietogonzalez@gmail.com>
2022-11-28 13:11:36 -0500

    Update init.rb

--- init.rb
+++ init.rb
@@ -4,9 +4,9 @@ Redmine::Plugin.register :fablab_plugin do
   name 'Fablab Plugin'
   author 'Kai Nieto Gonzalez'
   description 'Utilities for RIT FabLab'
-  version '0.0.2'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  version '1.0.0'
+  url 'https://github.com/Kai-Nalu/redmine-fablab-plugin'
+  author_url 'https://github.com/Kai-Nalu'
   
   menu :admin_menu, :workflow_prompts, { controller: 'workflow_prompts', action: 'index' }, caption: 'Workflow Prompts'
 end
